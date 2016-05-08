ddf5eb1a997386065ad61b5815c21f67b298dd85
Andrew54757 <andrews54757@gmail.com>
2016-05-08 13:22:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+// Config
+var port = 8080
+
+
+
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static('./client'));
-var port = process.env.port;
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
 var serverport = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || port;
 if (process.env.OPENSHIFT_NODEJS_IP !== undefined) {
     http.listen( serverport, ipaddress, function() {
-        console.log('[DEBUG] Listening on *:' + serverport);
+        console.log('[CLient] Listening on *:' + serverport);
     });
 } else {
     http.listen( serverport, function() {
-        console.log('[DEBUG] Listening on *:' + port);
+        console.log('[Client] Listening on *:' + port);
     });
 }
